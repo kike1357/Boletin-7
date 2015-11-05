7428649016bc7eaa9768f582d6d23152e138dc6e
epastorizalorenzo <epastorizalorenzo@atanasoff24.danielcastelao.org>
2015-11-05 09:53:56 +0100

    commit GitHub

---
+++ src/boletin7_3/Boletin7_3.java
@@ -0,0 +1,18 @@
+
+package boletin7_3;
+
+/**
+ *
+ * @author epastorizalorenzo
+ */
+public class Boletin7_3 {
+
+   
+    public static void main(String[] args) {
+        int numero;
+        Simbolos n= new Simbolos();
+        numero = n.setNumero();
+        n.comprobacion(numero);
+    }
+    
+}
